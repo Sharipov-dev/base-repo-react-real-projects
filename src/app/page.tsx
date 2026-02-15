ee7f4f548a868b5fa54cb322cf929d3c74067666
Arthur Sharipov <sharipov@sheridancollege.ca>
2026-02-15 14:07:33 -0500

    Delete all the files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/login');
+  return <div>HomePage</div>;
 }
--- src/features/auth/api/auth.server.ts
+++
@@ -1,7 +0,0 @@
-import 'server-only';
-
-export {
-  getSession,
-  requireSession,
-  getUser,
-} from '@/shared/lib/auth/session.server';
